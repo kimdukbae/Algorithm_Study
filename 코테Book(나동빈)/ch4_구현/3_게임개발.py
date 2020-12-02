9b4437313d81d705492dce5e78d47344f45c18f2
kimdukbae <han885404@naver.com>
2020-12-02 23:41:16 +0900

    2020/12/02 commit

--- 코테Book(나동빈)/ch4_구현/3_게임개발.py
+++ 코테Book(나동빈)/ch4_구현/3_게임개발.py
@@ -1,12 +1,49 @@
-# 내 풀이
 N, M = map(int, input().split())
-x, y, direction = map(int, input().split())
-dir = [[-1, 0], [0, -1], [1, 0], [0, 1]]
-board = [0 for i in range(N)]
+r, c, direction = map(int, input().split())
+visited = [[0] * M for _ in range(N)]
+board = [[0] * M for _ in range(N)]
 
 for i in range(N):
     board[i] = list(map(int, input().split()))
 
+def turn_left():
+    global direction
+    direction -= 1
+    if direction < 0:
+        direction = 3
+
+# 북, 동, 남, 서
+dir = [[-1, 0], [0, 1], [1, 0] ,[0, -1]]
+ans = 0
+turn_time = 0
+
 while True:
+    turn_left()
+    nr = r + dir[direction][0]
+    nc = c + dir[direction][1]
 
+    if visited[nr][nc] == 0 and board[nr][nc] == 0:
+        visited[nr][nc] = 1
+        r = nr
+        c = nc
+        ans += 1
+        turn_time = 0
+        continue
 
+    else:
+        turn_time += 1
+
+    if turn_time == 4:
+        nr = r - dir[direction][0]
+        nc = c - dir[direction][1]
+
+        if board[nr][nc] == 0:
+            r = nr
+            c = nc
+
+        else:
+            break
+
+        turn_time = 0
+
+print(ans)
--- 프로그래머스/SkillCheck/2.py
+++ 프로그래머스/SkillCheck/2.py
@@ -1,29 +1,29 @@
-def solution(x):
-    answer = True
-    s = str(x)
-
-    if x < 1 or x > 10000:
-        answer = False
-        return answer
-
-    else:
-        divide = 0
-        for i in range(len(s)):
-            divide += int(s[i])
-
-        if x % divide != 0:
-            answer = False
-            return answer
-
-    return answer
-
-
-res1 = solution(10)
-res2 = solution(12)
-res3 = solution(11)
-res4 = solution(13)
-
-print(res1)
-print(res2)
-print(res3)
-print(res4)
+# def solution(x):
+#     answer = True
+#     s = str(x)
+#
+#     if x < 1 or x > 10000:
+#         answer = False
+#         return answer
+#
+#     else:
+#         divide = 0
+#         for i in range(len(s)):
+#             divide += int(s[i])
+#
+#         if x % divide != 0:
+#             answer = False
+#             return answer
+#
+#     return answer
+#
+#
+# res1 = solution(10)
+# res2 = solution(12)
+# res3 = solution(11)
+# res4 = solution(13)
+#
+# print(res1)
+# print(res2)
+# print(res3)
+# print(res4)
