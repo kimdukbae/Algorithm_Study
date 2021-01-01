a369ec5d2f6b80b4991790fc6a73e5906a59c72f
kimdukbae <han885404@naver.com>
2021-01-01 15:50:34 +0900

    2021 commit

--- 프로그래머스/4_DFS_BFS/네트워크.py
+++ 프로그래머스/4_DFS_BFS/네트워크.py
@@ -1,1 +1,22 @@
-print("why github?")
+def dfs(computers, visited, start):
+    record = [start]
+    while record:
+        j = record.pop()
+        if visited[j] == 0:
+            visited[j] = 1
+        for i in range(len(computers)):
+            if computers[j][i] == 1 and visited[i] == 0:
+                record.append(i)
+
+def solution(n, computers):
+    answer = 0
+    visited = [0 for _ in range(n)]
+    start = 0
+    while 0 in visited:
+        if visited[start] == 0:
+            dfs(computers, visited, start)
+            answer += 1
+        start += 1
+    return answer
+
+print(solution(3, [[1,1,0], [1,1,1], [0,1,1]]))
