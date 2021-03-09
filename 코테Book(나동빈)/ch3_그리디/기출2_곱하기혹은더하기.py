0d6f2568abb7742bac9d80af360762c1015e6125
kimdukbae <han885404@naver.com>
2021-03-09 14:36:13 +0900

    2021

--- 코테Book(나동빈)/ch3_그리디/기출2_곱하기혹은더하기.py
+++ 코테Book(나동빈)/ch3_그리디/기출2_곱하기혹은더하기.py
@@ -4,6 +4,7 @@ result = int(S[0])
 for i in range(1, len(S)):
     target = int(S[i - 1])
     addOrPlus = int(S[i])
+
     if target == 0 or target == 1:
         result += addOrPlus
     elif 2 <= target <= 9:
