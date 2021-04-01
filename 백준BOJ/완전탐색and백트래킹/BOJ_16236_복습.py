0bb268d1f21b01e98830517d90905a3ea7b62028
kimdukbae <han885404@naver.com>
2021-04-01 14:04:25 +0900

    2021

--- 백준BOJ/완전탐색and백트래킹/BOJ_16236_복습.py
+++ 백준BOJ/완전탐색and백트래킹/BOJ_16236_복습.py
@@ -1,11 +1,2 @@
-class Node:
-    def __init__(self, x, y, size):
-        self.x = x
-        self.y = y
-        self.size = size
+import sys
 
-
-node1 = Node(0, 1, 10)
-node2 = Node(1, 2, 5)
-print(node1.x, node1.y, node1.size)
-print(node2.x, node2.y, node2.size)
