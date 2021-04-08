34c3c8753e49930129f99b39b6e0b89c4fd21ab7
kimdukbae <han885404@naver.com>
2021-04-08 22:46:52 +0900

    2021

--- 백준BOJ/그리디/BOJ_11047_동전0.py
+++ 백준BOJ/그리디/BOJ_11047_동전0.py
@@ -15,3 +15,5 @@ for i in range(N - 1, -1, -1):
     K %= coin[i]
 
 print(ans)
+
+#  "i ≥ 2인 경우에 Ai는 Ai-1의 배수" --> 이 조건때문에 K가 무조건 나눠떨어지게 된다.
---
+++ 프로그래머스/3_그리디/5_섬연결하기2.py
@@ -0,0 +1,33 @@
+# 섬과 섬 연결하기
+def union(parent, a, b):
+    A = find(parent, a)
+    B = find(parent, b)
+
+    if A < B:
+        parent[B] = A
+    else:
+        parent[A] = B
+
+
+# 섬에 사이클 생기는지 판단
+def find(parent, x):
+    if parent[x] == x:
+        return x
+    return find(parent, parent[x])
+
+
+def solution(n, costs):
+    parents = [i for i in range(n)]
+    costs = sorted(costs, key=lambda x: x[2])
+
+    answer = 0
+    while costs:
+        city1, city2, cost = costs.pop(0)
+        if find(parents, city1) != find(parents, city2):
+            union(parents, city1, city2)
+            answer += cost
+
+    return answer
+
+
+print(solution(4, [[0, 1, 1], [0, 2, 2], [1, 2, 5], [1, 3, 1], [2, 3, 8]]))
