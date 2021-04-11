dc58b3b2ab38a2746776b5846d82c48a9c3f2c0a
kimdukbae <han885404@naver.com>
2021-04-12 02:23:37 +0900

    2021

---
+++ 백준BOJ/그리디2/BOJ_10775_공항.py
@@ -0,0 +1,41 @@
+import sys
+
+# 비행기를 도킹할 때 항상 넣을 수 있는 가장 큰 수의 게이트에 도킹해야함. (최대 도킹 수를 구해야하므로)
+input = sys.stdin.readline
+G = int(input())
+P = int(input())
+docking = list(int(input()) for _ in range(P))
+parents = [i for i in range(G + 1)]
+ans = 0
+
+
+def find(parent, x):
+    if parent[x] == x:
+        return x
+    parent[x] = find(parent, parent[x])
+    return parent[x]  # 경로압축
+
+
+def union(parent, a, b):
+    rootA = find(parent, a)
+    rootB = find(parent, b)
+
+    if rootA < rootB:
+        parent[rootB] = rootA
+    else:
+        parent[rootA] = rootB
+
+
+for d in docking:
+    root = find(parents, d)
+    # 0일 때 끝나는 이유 -> 비행기가 어느 게이트에도 도킹할 수 없으면 공항 폐쇄되고,
+    # 이후 어떤 비행기도 도착할 수 없기 때문에 더이상 탐색할 필요가 X
+    if root == 0:
+        break
+
+    # union(parents, d, d - 1) --> (Ex) 2-3 연결되어있고 1은 비어있음. d=3이면 1-2-3 연결되어야 하는데
+    # 3의 부모인 2와 1을 연결해야하는데, 위 코드는 3과 2를 연결해서 오류발생!
+    union(parents, root, root - 1)
+    ans += 1
+
+print(ans)
--- 백준BOJ/그리디2/BOJ_1434_책정리.py
+++ 백준BOJ/그리디2/BOJ_1434_책정리.py
@@ -5,6 +5,5 @@ N, M = map(int, input().split())
 boxes = list(map(int, input().split()))
 books = list(map(int, input().split()))
 
-# 장난치나 이게 문제냐
 print(sum(boxes) - sum(books))
 
