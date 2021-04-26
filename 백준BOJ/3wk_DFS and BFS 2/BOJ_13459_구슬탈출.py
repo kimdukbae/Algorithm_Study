abae1b48e5ce19dd4608e5fbce23a5fae673f8b0
kimdukbae <han885404@naver.com>
2021-04-26 21:45:38 +0900

    2021

--- 백준BOJ/3wk_DFS and BFS 2/BOJ_13459_구슬탈출.py
+++ 백준BOJ/3wk_DFS and BFS 2/BOJ_13459_구슬탈출.py
@@ -6,16 +6,3 @@ row, col = map(int, input().split())
 board = [list(input().rstrip()) for _ in range(row)]
 
 dir = [[-1, 0], [1, 0], [0, -1], [0, 1]]
-redX, redY, blueX, blueY = 0, 0, 0, 0
-
-for r in range(row):
-    for c in range(col):
-        if board[r][c] == 'R':
-            redX, redY = r, c
-        elif board[r][c] == 'B':
-            blueX, blueY = r, c
-
-
-def bfs():
-    q_red = deque([])
-    q_blue = deque([])
