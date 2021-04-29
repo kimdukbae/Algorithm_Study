ee5196b0bc3a7e321db8c50305244e0edd6c0201
kimdukbae <han885404@naver.com>
2021-04-29 17:01:19 +0900

    2021

--- 백준BOJ/5wk_정렬/BOJ_5052_전화번호목록.py
+++ 백준BOJ/5wk_정렬/BOJ_5052_전화번호목록.py
@@ -4,19 +4,18 @@ input = sys.stdin.readline
 T = int(input())
 for _ in range(T):
     N = int(input())
-    phone_num = [input() for _ in range(N)]
+    phone_num = [input().rstrip() for _ in range(N)]
 
+    # 전화번호 목록 오름차순 정렬
     phone_num.sort()
+
     flag = False
-    basis = ''
-    for i in range(N):
-        if not flag:
-            basis = phone_num.pop(0)
-        if phone_num:
-            for pn in phone_num:
-                if basis == pn[0:len(basis)]:
-                    flag = True
-                    break
+    for i in range(N - 1):
+        # 비교 번호가 바로 뒤에 있는 번호의 접두어인 경우인지 확인
+        # --> 정렬되었기에 이 조건만으로도 일관성을 판단할 수 있다.
+        if phone_num[i] == phone_num[i + 1][0:len(phone_num[i])]:
+            flag = True
+            break
 
     if flag:
         print('NO')
