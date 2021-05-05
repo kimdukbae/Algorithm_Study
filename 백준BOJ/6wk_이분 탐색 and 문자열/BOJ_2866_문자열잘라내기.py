fdab671ea758ec0b47104077d988d465f17c342e
kimdukbae <han885404@naver.com>
2021-05-06 01:47:26 +0900

    2021

--- 백준BOJ/6wk_이분 탐색 and 문자열/BOJ_2866_문자열잘라내기.py
+++ 백준BOJ/6wk_이분 탐색 and 문자열/BOJ_2866_문자열잘라내기.py
@@ -1,3 +1,31 @@
+# 왜 시간 초과 발생하는지 모르겠음...
 import sys
 
 input = sys.stdin.readline
+r, c = map(int, input().split())
+words = [list(input().rstrip()) for _ in range(r)]
+
+
+def check():
+    cnt = 0
+    for _ in range(r - 1):
+        del words[0]
+        delete_words = list(map(str, zip(*words)))
+        delete_words = set(delete_words)
+        if len(delete_words) != c:
+            return cnt
+        cnt += 1
+
+    return cnt
+
+
+print(check())
+
+# 추가 테스트 케이스
+# 6 6
+# qwerqy
+# asdbgh
+# zxcvbn
+# aceeda
+# abdbza
+# cbedqc
--- 백준BOJ/6wk_이분 탐색 and 문자열/BOJ_9935_문자열폭발.py
+++ 백준BOJ/6wk_이분 탐색 and 문자열/BOJ_9935_문자열폭발.py
@@ -1,3 +1,18 @@
 import sys
 
 input = sys.stdin.readline
+string = input().rstrip()
+bomb = input().rstrip()
+stack = []
+
+for i in range(len(string)):
+    stack.append(string[i])
+    # stack[-x:] -> 문자열의 오른쪽에서부터 x번째 ~ 마지막 까지 잘라낸다.
+    # 문자열 인덱스 슬라이싱 활용을 잘하자!
+    if stack[-1] == bomb[-1] and ''.join(stack[-len(bomb):]) == bomb:
+        del stack[-len(bomb):]
+
+if stack:
+    print(''.join(stack))
+else:
+    print('FRULA')
